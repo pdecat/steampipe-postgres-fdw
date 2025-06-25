433f7e89ef684fa83c05f8f0b6e89bb52e47e3f9
Patrick Decat <pdecat@gmail.com>
2025-06-25 08:26:12 +0200

    fix: support PostgreSQL 18

--- fdw/query.c
+++ fdw/query.c
@@ -528,6 +528,9 @@ findPaths(PlannerInfo *root, RelOptInfo *baserel, List *possiblePaths,
             NULL, /* default pathtarget */
 #endif
             nbrows,
+#if PG_VERSION_NUM >= 180000
+            0,
+#endif
             startupCost,
 #if PG_VERSION_NUM >= 90600
             nbrows * baserel->reltarget->width,
@@ -577,7 +580,11 @@ deparse_sortgroup(PlannerInfo *root, Oid foreigntableid, RelOptInfo *rel)
 
     if ((expr = fdw_get_em_expr(ec, rel)))
     {
+#if PG_VERSION_NUM >= 180000
+      md->reversed = (key->pk_cmptype == BTGreaterStrategyNumber);
+#else
       md->reversed = (key->pk_strategy == BTGreaterStrategyNumber);
+#endif
       md->nulls_first = key->pk_nulls_first;
       md->key = key;
 
